d2bf9865ea575f34d8626ee5c7cb22eae93c9160
Michael Chen <mikaloramik@gmail.com>
2014-03-15 22:09:18 -0400

    add local angular, refactor for new route mechanism

--- client/js/app.js
+++ client/js/app.js
@@ -1,6 +1,13 @@
-angular.module('easel-chat', [])
-    .config(['$routeProvider', function($routeProvider) {
+var easelApp = angular.module('easel-chat', ['EaselControllers', 'ngRoute']);
+
+easelApp.factory('UserData', function() {
+    return {userName: "", gameName: ""};
+});
+
+easelApp.config(['$routeProvider', function($routeProvider) {
         $routeProvider.
-        when('/logIn', { templateUrl: '/partials/logIn', controller: LogInCtrl }).
+        when('/logIn', { templateUrl: '/partials/logIn', controller: 'LogInCtrl' }).
+        when('/gameRoom', { templateUrl: '/partials/gameRoom', controller: 'GameRoomCtrl' }).
         otherwise({ redirectTo: '/logIn'});
     }]);
+
--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,3 +1,18 @@
- function LogInCtrl($scope) {
+var easelControllers = angular.module('EaselControllers', []);
+
+easelControllers.controller('LogInCtrl', ['$scope', '$location', 'UserData', function($scope, $location, UserData) {
+  $scope.data = UserData;
   console.log("In Login Ctrl");
- }
+
+  $scope.joinGame = function() {
+    console.log("tried to join game");
+    console.log($scope.data);
+    $location.path("/gameRoom");
+  }
+ }]);
+
+easelControllers.controller('GameRoomCtrl', ['$scope', 'UserData', function($scope, UserData) {
+    console.log("in gameroom");
+    $scope.data = UserData;
+    console.log($scope.data);
+}]);
